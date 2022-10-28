ad39a7c4dbe783bd989eaaab539c4c67b532946c
scizen9 <neilljd@gmail.com>
2022-10-28 12:45:44 -0700

    Robustify rc_focus.py

--- utils/rc_focus.py
+++ utils/rc_focus.py
@@ -10,4 +10,11 @@ def temp_to_focus(current_temp):
         names=['temp', 'focus'], dtype=[float, float], delimiter=',',
         skip_header=True)
     index = np.where(focustable['temp'] == round(current_temp, 1))
+    if len(index[0]) <= 0:
+        if current_temp < focustable['temp'].min():
+            index = np.where(focustable['temp'] == focustable['temp'].min())
+        elif current_temp > focustable['temp'].max():
+            index = np.where(focustable['temp'] == focustable['temp'].max())
+        else:
+            index = ([0])
     return focustable['focus'][index][0]
