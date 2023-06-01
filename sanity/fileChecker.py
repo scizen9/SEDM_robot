3a42f19c56420df8eeafc1f4a4e7a794f4b70202
scizen9 <neilljd@gmail.com>
2023-06-01 01:45:14 -0700

    Fix find files algorithm in fileChecker.py

--- sanity/fileChecker.py
+++ sanity/fileChecker.py
@@ -40,21 +40,22 @@ class Checker:
             return {'elaptime': time.time()-start,
                     'error': "keywords are not in dict form"}
 
+        n_keys = len(keywords)
+        print("Checking %d keywords" % n_keys)
         nfiles = 0
         for f in files:
-            key_there = 0
-            n_keys = 0
+            keys_there = 0
             header = fits.getheader(f)
             for k, v in keywords.items():
                 n_keys += 1
                 if k.upper() in header:
                     if isinstance(v, str):
                         if v.lower() in header[k.upper()]:
-                            key_there += 1
+                            keys_there += 1
                     elif isinstance(v, float) or isinstance(v, int):
                         if v == header[k.upper()]:
-                            key_there += 1
-            if key_there == n_keys:
+                            keys_there += 1
+            if keys_there == n_keys:
                 img_list.append(f)
                 nfiles += 1
 
